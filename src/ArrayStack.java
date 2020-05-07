264d5d5accf4362cc2d638bf70efc07530619541
koushik <koushik>
2020-05-07 18:41:38 +0800

    Update : Push and pop complete

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -34,8 +34,26 @@ public class ArrayStack {
        return employee;
     }
 
+    public Employee peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+
+        return stack[top - 1];
+    }
+
+    public int size(){
+        return top;
+    }
+
     public boolean isEmpty(){
 
         return top == 0;
+    }
+
+    public void printStack(){
+        for(int i = top - 1 ; i>=0 ; i--){
+            System.out.println(stack[i]);
+        }
     }
 }
