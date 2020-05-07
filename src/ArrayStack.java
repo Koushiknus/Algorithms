68e5c6d3130691454cfc148df3bf85979a51ad07
koushik <koushik>
2020-05-07 18:03:04 +0800

    Update : Push and Pop implementation

---
+++ src/ArrayStack.java
@@ -0,0 +1,41 @@
+import java.util.EmptyStackException;
+
+public class ArrayStack {
+
+    private Employee[] stack;
+    private int top;
+
+    public ArrayStack(int capacity){
+        stack = new Employee[capacity];
+    }
+
+    public void push(Employee employee){
+        // stack is full
+        //Time complexity in this situation is O[n] as you are copying all the elements
+        if(top == stack.length){
+           // need to resize back array
+           Employee[] newArray = new Employee[2*stack.length];
+           //copy stack elements from 0 position to new Array starting from 0 position
+           System.arraycopy(stack,0,newArray,0,stack.length);
+           // Now assign the resized new Array to original one
+           stack = newArray;
+        }
+        // If space is available , just insert it
+        // Time Complexity is O[1] as you have space you dont bother
+        stack[top++] = employee;
+    }
+
+    public Employee pop(){
+       if(isEmpty()) {
+           throw new EmptyStackException();
+       }
+       Employee employee = stack[--top];
+       stack[top] = null;
+       return employee;
+    }
+
+    public boolean isEmpty(){
+
+        return top == 0;
+    }
+}
