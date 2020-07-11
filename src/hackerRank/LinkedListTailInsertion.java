078716461a1ee047815ad239f39f8ba383277d57
koushik <koushik>
2020-07-11 22:04:28 +0800

    Update : LinkedList Tail Insertion and Run Time Polymorphism added

---
+++ src/hackerRank/LinkedListTailInsertion.java
@@ -0,0 +1,43 @@
+package hackerRank;
+
+public class LinkedListTailInsertion {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+
+    }
+
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        // If no node is available , create the first node and insert it
+        if(head == null){
+            head = new SinglyLinkedListNode(data);
+            head.data = data;
+        }else{
+            SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+            newNode.data = data;
+            SinglyLinkedListNode currentNode = head;
+            //Move to the end of Linked List through traversal
+            while(currentNode.next != null){
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+        return head;
+
+    }
+}
